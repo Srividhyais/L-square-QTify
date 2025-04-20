8867ec6bcac7d58544f3e056dc7aecc3f68c5775
Srividhya Radhakrishnan <vidhya@Srividhyas-MacBook-Air.local>
2025-04-19 22:47:06 -0400

    initial

---
+++ qtify/src/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.module.css';  
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button className={styles.Button} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
